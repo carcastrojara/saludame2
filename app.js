77c8531d5b4e17beaef6834193454179ce3d7bec
Carlos <carcastrojara@gmail.com>
2023-02-28 15:38:01 -0300

    second commit

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/makers/:nombre', (req, res) => {
-    //http://localhost:3000/makers/juan
+    //http://localhost:3000/makers/Juan
+    const str = req.params.nombre;
+const str2 = str.charAt(0).toUpperCase() + str.slice(1);
 
     if (!req.params.nombre || !req.params.nombre.length ) {
         res.send(`<h1>Hola desconocido!</h1>`);
     }else{
-        res.send(`<h1>Hola ${req.params.nombre}!</h1>`);
+        res.send(`<h1>Hola ${str2}!</h1>`);
     }
 });
 
